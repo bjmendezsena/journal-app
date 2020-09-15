ccefcea5b26170e2771f95ceb88b4859c8440275
Briaulin Mendez <b.j.mendezsena@gmail.com>
2020-09-15 10:36:41 +0200

    Docs agregado

---
+++ docs/precache-manifest.cb72064943e1976aee3a9ada38bb4d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb8ad868fbd51fce70bb0373acf9a4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc9367d04c3f93b7abab",
+    "url": "/static/css/main.a9d316e9.chunk.css"
+  },
+  {
+    "revision": "adb7a9105a5528b05890",
+    "url": "/static/js/2.7e0cd57a.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.7e0cd57a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9367d04c3f93b7abab",
+    "url": "/static/js/main.2b83a04b.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
